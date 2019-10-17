bf0d4275900884af6bf159a07ada54581479902a
Neha Chetry <nehachetry@Nehas-MacBook-Pro-4.local>
2019-10-16 18:32:35 -0700

    Add LitPaths and reove LAData

---
+++ LitPaths.py
@@ -0,0 +1,24 @@
+# pip install googlemaps
+import googlemaps
+import urllib
+import json
+import requests
+from key import key
+directionsurl= https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=key
+
+
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print data
+
+
+
+
+
+
+
+
+
+	
+
+
